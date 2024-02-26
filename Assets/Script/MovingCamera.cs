f3695f9cfd793e87f7e7e050a0eb6a20995bacd5
Christopher Pani <christopher.pani@gmail.com>
2024-02-26 16:44:57 +0100

    Movimento della videocamera ottimizzato

--- Assets/Script/MovingCamera.cs
+++ Assets/Script/MovingCamera.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class MovingCamera : MonoBehaviour
 {
-     Transform player;
+    Transform player;
     public Vector2 minBoundary, maxBoundary;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
     void Update()
     {
-        Vector2 playerPosition = player.position;
+        Vector3 playerPosition = player.position;
         playerPosition.x = Mathf.Clamp(playerPosition.x, minBoundary.x, maxBoundary.x);
         playerPosition.y = Mathf.Clamp(playerPosition.y, minBoundary.y, maxBoundary.y);
+        playerPosition.z = transform.position.z;
         transform.position = playerPosition;
     }
 }
